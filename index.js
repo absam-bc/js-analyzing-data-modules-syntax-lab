d40448277e7c12df2f9f614361f1489cbc079c58
Abdulsalam Ahmed <abdisalanahmed95@gmail.com>
2026-07-08 15:11:29 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (const userArray of args) {
+    combinedObject.users = [...combinedObject.users, ...userArray];
+  }
+
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+module.exports = combineUsers;
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
